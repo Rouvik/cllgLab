5fff92c744385afbdfe383d2af6ff7202c36da4c
Rouvik Maji <61477057+Rouvik@users.noreply.github.com>
2025-03-18 22:40:56 +0530

    Update q1.c improved performance

--- Assign4/q1.c
+++ Assign4/q1.c
@@ -2,14 +2,16 @@
 
 int main()
 {
-	printf("Enter a, d, n: "); int a, d, n;
-	scanf("%d %d %d", &a, &d, &n);
+        printf("Enter a, d, n: "); int a, d, n;
+        scanf("%d %d %d", &a, &d, &n);
 
-	for(int i = 0; i < n; i++)
-	{
-		printf("%d ", a + i * d);
-	}
-	puts("");
+	int s = a;
+        for(int i = 0; i < n; i++)
+        {
+                printf("%d ", s);
+                s += d;
+        }
+        printf("\n");
 
-	return 0;
+        return 0;
 }
