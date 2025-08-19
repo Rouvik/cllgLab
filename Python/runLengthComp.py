8ea1f9e3391f50bdc150c816d164225e543ef755
Rouvik Maji <majirouvik@gmail.com>
2025-08-19 13:02:03 +0530

    Update

---
+++ Python/runLengthComp.py
@@ -0,0 +1,17 @@
+# Example string AAAABBBBBCCD
+# Example output: A4B5C2D1
+
+inp = input("Enter a string to compress: ")
+ch = inp[0]
+i = 0
+count = 0
+while i < len(inp):
+	if inp[i] == ch:
+		count += 1
+	else:
+		print(ch, count, sep='', end='')
+		count = 1
+		ch = inp[i]
+	i += 1
+
+print() # Waste a new line to make it terminal friendly
