c8ddf7ea5e053d959cf74d0a4fa75c7e62887789
Rouvik Maji <majirouvik@gmail.com>
2025-09-02 12:33:30 +0530

    Added queue and stack in python

--- Python/queue.py
+++ Python/queue.py
@@ -2,16 +2,16 @@ container = []
 
 if __name__ == "__main__":
 	while True:
-		ch = int(input("Enter choice:\n1) Push elem\n2) Pop elem\n3)Print\n4) Quit\n> "))
+		ch = int(input("Enter choice:\n1) Insert elem\n2) Remove elem\n3) Print\n4) Quit\n> "))
 		match ch:
 			case 1:
-				elem = int(input("Enter element to push: "))
-				container.append(elem)
+				elem = int(input("Enter an element to insert: "))
+				container.insert(0, elem)
 				print(container)
 
 			case 2:
 				elem = container.pop()
-				print("Popped: ", elem)
+				print("Removed: ", elem)
 				print(container)
 
 			case 3:
@@ -22,4 +22,3 @@ if __name__ == "__main__":
 
 			case _:
 				print("Unknown case")
-
