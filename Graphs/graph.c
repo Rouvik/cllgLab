8d464ff70460c854890b324116944cedacfa7b5a
Rouvik Maji <majirouvik@gmail.com>
2025-12-03 18:13:32 +0530

    Update and fix Graph DFS and cycle detect

--- Graphs/graph.c
+++ Graphs/graph.c
@@ -4,7 +4,7 @@
 
 #define INDEX_ADJ(adj, i, j) adj[j * adj[0] + i + 1]
 
-int * Graph_init(int maxRadius)
+int *Graph_init(int maxRadius)
 {
 	int *mat = (int *)malloc(maxRadius * maxRadius * sizeof(int));
 	mat[0] = maxRadius;
@@ -17,9 +17,9 @@ void Graph_free(int **adjMat)
 	*adjMat = NULL;
 }
 
-void Graph_addEdge(int* adj, int v1, int v2, bool bidirectional)
+void Graph_addEdge(int *adj, int v1, int v2, bool bidirectional)
 {
-	if(v1 > adj[0] || v2 > adj[0])
+	if (v1 > adj[0] || v2 > adj[0])
 	{
 		printf("Error, edge greater than accomodable by graph\n");
 		return;
@@ -27,13 +27,14 @@ void Graph_addEdge(int* adj, int v1, int v2, bool bidirectional)
 
 	INDEX_ADJ(adj, v1, v2) = 1;
 
-	if(bidirectional) INDEX_ADJ(adj, v2, v1) = 1;
+	if (bidirectional)
+		INDEX_ADJ(adj, v2, v1) = 1;
 }
 
 void Graph_print(int *adj)
 {
 	fputs("   ", stdout);
-	for(int i = 0; i < adj[0]; i++)
+	for (int i = 0; i < adj[0]; i++)
 	{
 		printf("%d ", i);
 	}
@@ -41,17 +42,17 @@ void Graph_print(int *adj)
 	putchar('\n');
 
 	fputs("   ", stdout);
-	for(int i = 0; i < adj[0]; i++)
+	for (int i = 0; i < adj[0]; i++)
 	{
 		fputs("--", stdout);
 	}
 
 	putchar('\n');
 
-	for(int i = 0; i < adj[0]; i++)
+	for (int i = 0; i < adj[0]; i++)
 	{
 		printf("%d| %d ", i, INDEX_ADJ(adj, i, 0));
-		for(int j = 1; j < adj[0]; j++)
+		for (int j = 1; j < adj[0]; j++)
 		{
 			printf("%d ", INDEX_ADJ(adj, i, j));
 		}
@@ -59,29 +60,30 @@ void Graph_print(int *adj)
 	}
 }
 
-bool Graph_cycleDetect(int *adj, int v, int *visited)
+bool Graph_cycleDetect(int *adj, int p, int v, int *visited)
 {
-	if(v > adj[0])
+	// make sure not to start with non existent vertices
+	if (v > adj[0])
 	{
 		printf("Error, vertex greater than maxRadius\n");
 		return false;
 	}
 
-	visited[v] = true;	// mark as visited, on call
+	visited[v] = true; // mark as visited, on call
 
-	bool detected = false;
-	for(int i = 0; i < adj[0]; i++)
+	for (int i = 0; i < adj[0]; i++)
 	{
-		if(i == v) continue;	// dont try to visit self
+		if (i == v || i == p)
+			continue; // dont try to visit self or consider its previous
 
-		if(INDEX_ADJ(adj, i, v))
+		if (INDEX_ADJ(adj, v, i)) // go through neighbours and visit, if visited -> CYCLE :)
 		{
-			if(visited[i]) return true;
-			detected |= Graph_cycleDetect(adj, i, visited);
+			if (visited[i] || Graph_cycleDetect(adj, v, i, visited))
+				return true;
 		}
 	}
 
-	return detected;
+	return false;
 }
 
 int main(int argc, const char *argv[])
@@ -89,14 +91,14 @@ int main(int argc, const char *argv[])
 	int *graph = Graph_init(5);
 	Graph_addEdge(graph, 0, 1, true);
 	Graph_addEdge(graph, 1, 2, true);
-	Graph_addEdge(graph, 2, 3, true);
+	Graph_addEdge(graph, 2, 0, true);
 
 	Graph_print(graph);
 	putchar('\n');
 
 	int visited[5];
 
-	printf("Has cycles? %s\n", Graph_cycleDetect(graph, 0, visited) ? "TRUE" : "FALSE");
+	printf("Has cycles? %s\n", Graph_cycleDetect(graph, -1, 0, visited) ? "TRUE" : "FALSE");
 
 	Graph_free(&graph);
 
