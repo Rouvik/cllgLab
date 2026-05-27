689fd5c45c62863d96197d8276f65ff55f8968a7
Rouvik Maji <majirouvik@gmail.com>
2026-05-27 13:49:06 +0530

    Update verilog

--- Verilog/component/a5/magComp.v
+++ Verilog/component/a5/magComp.v
@@ -1,9 +1,13 @@
-module magComp (input wire [3:0] A, input wire [3:0] B, output wire gt, output wire lt, output wire eq);
-    wire x3 = (A[3] ~^ B[3]);
-    wire x2 = (A[2] ~^ B[2]);
-    wire x1 = (A[1] ~^ B[1]);
-
-    assign eq = &(~(A ^ B));
-    assign gt = (A[3] & ~B[3]) | (x3 & A[2] & ~B[2]) | (x3 & x2 & A[1] & ~B[1]) | (x3 & x2 & x1 & A[0] & ~B[0]);
-    assign lt = eq ~| gt;
+module magComp (input wire [3:0] A, input wire [3:0] B, output reg gt, output reg lt, output reg eq);
+	always @(*) begin
+		if (A > B) begin
+			gt = 1; lt = 0; eq = 0;
+		end
+		else if (A < B) begin
+			gt = 0; lt = 1; eq = 0;
+		end
+		else begin
+			gt = 0; lt = 0; eq = 1;
+		end
+	end
 endmodule
