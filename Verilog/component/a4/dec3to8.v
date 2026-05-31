38361d8003c662c408c8930db59d9e2434f72090
Rouvik Maji <majirouvik@gmail.com>
2026-06-01 02:12:41 +0530

    Fixed bug in Verilog

--- Verilog/component/a4/dec3to8.v
+++ Verilog/component/a4/dec3to8.v
@@ -1,10 +1,10 @@
 module dec3to8(input a, input b, input c, output wire [7:0] out, input en);
-	assign out[0] = ~a & ~b & ~c;
-	assign out[1] = ~a & ~b & c;
-	assign out[2] = ~a & b & ~c;
-	assign out[3] = ~a & b & c;
-	assign out[4] = a & ~b & ~c;
-	assign out[5] = a & ~b & c;
-	assign out[6] = a & b & ~c;
-	assign out[7] = a & b & c;
+	assign out[0] = en & ~a & ~b & ~c;
+	assign out[1] = en & ~a & ~b & c;
+	assign out[2] = en & ~a & b & ~c;
+	assign out[3] = en & ~a & b & c;
+	assign out[4] = en & a & ~b & ~c;
+	assign out[5] = en & a & ~b & c;
+	assign out[6] = en & a & b & ~c;
+	assign out[7] = en & a & b & c;
 endmodule
